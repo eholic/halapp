e19377d9fb2ec4e3bc647a80b35ca4807229b78a
eholic <task@mxxiv.net>
2024-09-16 19:53:10 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,67 @@
-import Image from "next/image";
+'use client';
+
+import {useState} from 'react';
+
+type kanaDict = {
+    [key: string]: string
+}
+
+const en2kana: kanaDict = {
+  "1": "ぬ", "2": "ふ", "3": "あ", "4": "う", "5": "え", "6": "お", "7": "や", "8": "ゆ", "9": "よ", "0": "わ", "-": "ほ", "^": "へ",
+  "q": "た", "w": "て", "e": "い", "r": "す", "t": "か", "y": "ん", "u": "な", "i": "に", "o": "ら", "p": "せ",
+  "a": "ち", "s": "と", "d": "し", "f": "は", "g": "き", "h": "く", "j": "ま", "k": "の", "l": "り", ";": "れ", ":": "け", "]": "む",
+  "z": "つ", "x": "さ", "c": "そ", "v": "ひ", "b": "こ", "n": "み", "m": "も", ",": "ね", ".": "る", "/": "め", "_": "ろ",
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [eng, setEng] = useState(false);
+  const [text, setText] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const keyDownHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    const key = e.key;
+    const code = e.code;
+
+    let speak = "";
+    let _eng = eng;
+
+    if (code.indexOf("Arrow") != -1) {
+      _eng = !_eng;
+      setEng(_eng);
+      if (_eng) {
+        speak = "English Mode";
+        setText("えいご");
+      }else{
+        speak = "日本語モード";
+        setText("にほんご");
+      }
+    } else {
+      if(_eng) {
+        speak = key;
+        setText(key.toUpperCase());
+      }else{
+        speak = en2kana[key.toLowerCase()];
+        setText(en2kana[key.toLowerCase()]);
+      }
+    }
+
+
+    // speak
+    const uttr = new SpeechSynthesisUtterance(speak);
+    if (_eng) {
+      uttr.lang = "en-US";
+    }else{
+      uttr.lang = "ja-JP";
+    }
+    window.speechSynthesis.speak(uttr);
+  }
+
+
+  return (
+    <div className="container h-dvh content-center"
+      tabIndex={0}
+      onKeyDown={keyDownHandler}>
+
+      <p className="text-9xl flex justify-center">{text}</p>
     </div>
   );
 }
